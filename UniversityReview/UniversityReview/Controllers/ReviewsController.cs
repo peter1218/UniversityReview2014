36be6eb037b2494ecf4754581db6119b567c378e
peter <peterl18@hotmail.com>
2015-02-28 11:43:02 -0800

    add Review controller and index view

---
+++ UniversityReview/UniversityReview/Controllers/ReviewsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityReview.Models;
+
+namespace UniversityReview.Controllers
+{  
+    public class ReviewsController : Controller
+    {
+        UniRatingDB _db = new UniRatingDB();
+        //
+        // GET: /Reviews/
+
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var university = _db.Universities.Find(restaurantId);
+            if (university != null)
+            {
+                return View(university);
+            }
+            return HttpNotFound();
+        }
+          protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
