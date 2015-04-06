ba5ee0f6bf903b43bfea9caf710d1e2d6016d1bf
peter0171 <peterL26@hotmail.com>
2015-04-05 20:42:51 -0700

    test

--- UniversityReview/UniversityReview/Models/University.cs
+++ UniversityReview/UniversityReview/Models/University.cs
@@ -7,6 +7,7 @@ namespace UniversityReview.Models
 {
     public class University
     {
+        //peter
         public int Id { get; set; }
         public string Name{get;set;}
         public string City{get;set;}
